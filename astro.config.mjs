ccf41ff1311325094df0ffeb860a470b18d699fe
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-11-25 15:44:08 -0400

    chore: adapter de vercel y auth-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import auth from "auth-astro";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +12,10 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
+    auth(),
   ],
+
+  output: "server",
+  adapter: vercel(),
 });
 
