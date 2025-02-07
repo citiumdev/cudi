21ab5c74d644f757a7661e46c433d4a1e0229171
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2025-02-07 19:34:57 -0400

    chore: paquetes actualizados

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import auth from "auth-astro";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), auth(), db()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    auth(),
+    db(),
+  ],
 
   output: "server",
   adapter: vercel(),
 });
+
